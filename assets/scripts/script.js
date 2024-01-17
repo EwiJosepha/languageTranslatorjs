7f6a20e70eff4308f1bdba29ce09d91204ffdfb0
ewi josepha <ewijosepha@gmail.com>
2024-01-17 16:21:10 +0100

    adding scripts

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,29 @@
 const selecttag = document.querySelectorAll("select");
-selecttag.forEach((tag) => {
+const fromText = document.querySelector(".from-text");
+
+const translationbtn = document.querySelector('button')
+selecttag.forEach((tag, id) => {
   for (const country_code in countries) {
+    //selecting english as language from and hindi as language to by default
+    let selected;
+    if(id ==0 && country_code == "en-US"){
+      selected = "selected"
+    }else if (id == 1 && country_code == "hi-IN" ){
+      selected = "selected"
+    }
     let countryAtCode = countries[country_code]
-    let options = `            <option value="hi-IN">${countryAtCode}</option>
+    let options = `<option value=${country_code} ${selected}>${countryAtCode}</option>
   `
-    console.log(country_code);
+  tag.insertAdjacentHTML("beforeend", options)
+  
   }
+})
+
+translationbtn.addEventListener("click", ()=>{
+let text = fromText.value
+let translatefrom = selecttag[0].value
+let translateto = selecttag[1].value
+console.log(translatefrom);
+console.log(translateto);
 })
 
